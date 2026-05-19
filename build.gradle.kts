a27a788d92f306bd2cd9371207e2e68ded772ceb
Andrii Kostiushyn <kaboom0202@gmail.com>
2026-05-19 21:14:13 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
-    id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
-    id("com.google.dagger.hilt.android") version "2.58" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("org.jetbrains.dokka") version "2.2.0" apply false
 }
 
